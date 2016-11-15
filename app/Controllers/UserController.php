abc558e6ec482501516296c7d9b6041897dcf9b3
Kaan Karaca <kaan94karaca@gmail.com>
2016-11-15 16:11:01 +0300

    models and pdo

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -7,13 +7,22 @@
  */
 namespace App\Controllers;
 
+use App\Models\User;
+use PDO;
+
 class UserController
 {
+    protected $db;
+
+    public function __construct(PDO $db)
+    {
+        $this->db = $db;
+    }
 
     public function index($response)
     {
-        return $response->withJson([
-            'test' => true
-        ]);
+        $users = $this->db->query('SELECT * FROM users')->fetchAll(PDO::FETCH_CLASS, User::class);
+
+        return $response->withJson($users);
     }
 }
---
+++ app/Model/User.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kaan
+ * Date: 11/15/2016
+ * Time: 3:07 PM
+ */
+namespace App\Models;
+
+class User{
+
+}
